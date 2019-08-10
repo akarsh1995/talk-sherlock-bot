08a453951e533c8312f2795b1e3726d73d906cdb
Akarsh <akarsh.1995.02@gmail.com>
2019-08-10 19:23:17 +0530

    cleaned dataset and in support vectors added.

--- seq2seq.py
+++ seq2seq.py
@@ -6,6 +6,8 @@ import pickle
 import os
 
 # Removes an annoying Tensorflow warning
+from model_interface.model_interface import get_test_input, ids_to_sentence
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
@@ -94,39 +96,6 @@ def translate_to_sentences(inputs, w_list, encoder=False):
                     list_of_strings[index] = list_of_strings[index] + " " + w_list[num_]
     list_of_strings = [string.strip() for string in list_of_strings]
     return list_of_strings
-
-
-def get_test_input(input_message, w_list, max_len):
-    encoderMessage = np.full(max_len, w_list.index('<pad>'), dtype='int32')
-    inputSplit = input_message.lower().split()
-    for index, word in enumerate(inputSplit):
-        try:
-            encoderMessage[index] = w_list.index(word)
-        except ValueError:
-            continue
-    encoderMessage[index + 1] = w_list.index('<EOS>')
-    encoderMessage = encoderMessage[::-1]
-    encoderMessageList = []
-    for num_ in encoderMessage:
-        encoderMessageList.append([num_])
-    return encoderMessageList
-
-
-def ids_to_sentence(ids_, w_list):
-    eos_token_index = w_list.index('<EOS>')
-    pad_token_index = w_list.index('<pad>')
-    my_str = ""
-    list_of_responses = []
-    for num_ in ids_:
-        if num_[0] == eos_token_index or num_[0] == pad_token_index:
-            list_of_responses.append(my_str)
-            my_str = ""
-        else:
-            my_str = my_str + w_list[num_[0]] + " "
-    if my_str:
-        list_of_responses.append(my_str)
-    list_of_responses = [i for i in list_of_responses if i]
-    return list_of_responses
 
 
 # Hyperparamters
